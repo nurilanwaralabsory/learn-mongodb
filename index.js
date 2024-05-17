1b07d75af8956023f76e7936c822b1c24ca8d8aa
nurilanwaralabsory <nurilanwaralabsory@gmail.com>
2024-05-17 21:11:05 +0700

    mengubah data berdasarkan filter atau id dengan mongoose

--- index.js
+++ index.js
@@ -22,7 +22,19 @@ const movieSchema = new mongoose.Schema({
 // membuat model database menggunakan schema yang telah dibuat diatas
 const Movie = mongoose.model("Movie", movieSchema);
 
-Movie.findOne({ year: { $gte: 2018 } })
+// Movie.updateOne({ title: "Parasite" }, { rating: 5 })
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+Movie.findByIdAndUpdate(
+  "6646fd0815b1154940a442a3",
+  { rating: 10 },
+  { new: true }
+)
   .then((result) => {
     console.log(result);
   })
